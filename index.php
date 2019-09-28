d1dc1eab9e06a4843395ab4aab6838cf54b298fe
hamzataougar <taougarhamza69@gmail.com>
2019-09-28 13:35:19 +0100

    Changement

--- index.php
+++ index.php
@@ -3,11 +3,12 @@
 
 $array = array("1" => "PHP code tester Sandbox Online",  
               "foo" => "bar", 5 , 5 => 89009, 
-              "case" => "Random Stuff: " . rand(100,999),
+              "fetch" => "Random Stuff: " . rand(100,999),
               "PHP Version" => phpversion()
               );
               
 foreach( $array as $key => $value ){
     echo $key."\t=>\t".$value."\n";
 }
+echo'test';
 
